0b6cdff96f5abc0b23c2bb9c2728ff0cd63f63c4
Chandran Goodchild <chandran@localhost.localdomain>
2017-04-28 01:58:04 +0200

    Quartus launch added to .bashrc

--- debian_setup/debian_setup_1.sh
+++ debian_setup/debian_setup_1.sh
@@ -31,6 +31,7 @@ sudo source /etc/bash.bashrc
 sudo echo "alias ls='ls --color=auto'" >> /etc/bash.bashrc
 sudo echo "alias ll='ls -la --color=auto'" >> /etc/bash.bashrc
 sudo echo "alias octave='octave --force-gui'" >> /etc/bash.bashrc
+sudo echo "alias quartus='cd && ./quartus/quartus/bin/quartus &'" >> /etc/bash.bashrc
 
 
 ###############################################
@@ -99,6 +100,7 @@ wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key a
 sudo apt-get -y update
 sudo apt-get -y install virtualbox-5.0 dkms
 sudo apt-get -y --force-yes install virtualbox-5.1
+
 
 # Make USB work in virtual box
 # https://askubuntu.com/questions/25596/how-to-set-up-usb-for-virtualbox
