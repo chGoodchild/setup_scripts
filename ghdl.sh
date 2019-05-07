15043a74bf160589e0cc7d7d5216b90d594e76ed
Chandran Florian Goodchild <ch.goodchild@gmail.com>
2019-05-07 21:37:21 +0200

    Added usage instructions

--- ghdl.sh
+++ ghdl.sh
@@ -11,3 +11,7 @@ sudo apt-get -y install libz-dev
 make
 make install
 sudo apt-get -y install gtkwave
+
+# Usage instructions:
+echo "Usage instructions:"
+wget -qO- https://github.com/chGoodchild/setup_scripts/raw/master/using_ghdl.sh
